0c1ea4c35ef9ee3a02c560b5ab3cbcfcad9c9b2f
Stella40k <bernardstella0704@gmail.com>
2025-10-16 19:47:23 -0300

    feat: middlewares para la proteccion de rutas del dashbord

--- src/models/reporte.model.js
+++ src/models/reporte.model.js
@@ -1,40 +1,36 @@
-import mongoose from "mongoose";
+    import mongoose from "mongoose";
 
-const ReporteSchema = new mongoose.Schema({
-  location: {
-    type: {
-      type: String,
-      enum: ["Point"],
-      required: true,
+    const ReporteSchema = new mongoose.Schema({
+    location: {
+        type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+        },
+        coordinates: {
+        type: [Number],
+        required: true,
+        },
     },
-    coordinates: {
-      type: [Number],
-      required: true,
+    tipoProblema: {
+        type: String,
+        required: true,
     },
-  },
-  tipoProblema: {
-    type: String,
-    required: true,
-  },
-  riesgoPercibido: {
-    type: Number,
-    required: true,
-  },
-  descripcion: {
-    type: String,
-    required: true,
-  },
-  fechaReporte: {
-    type: Date,
-    default: Date.now,
-  },
-});
+    riesgoPercibido: {
+        type: Number,
+        required: true,
+    },
+    descripcion: {
+        type: String,
+        required: true,
+    },
+    fechaReporte: {
+        type: Date,
+        default: Date.now,
+    },
+    });
 
-ReporteSchema.index({ location: "2dsphere" });
+    ReporteSchema.index({ location: "2dsphere" });
 
-const Reporte = mongoose.model("Reporte", ReporteSchema);
-<<<<<<< HEAD
-export default Reporte;
-=======
-export default Reporte;
->>>>>>> dev-vivi2
+    const Reporte = mongoose.model("Reporte", ReporteSchema);
+    export default Reporte;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes.js";
 import taskRoutes from "./task.routes.js";
-import statsRoutes from "./stats.route.js";
+import statsRoutes from "./stats.route.js"; // ESTE es el router que quiero proteger
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/tasks", taskRoutes);
-router.use("/stats", statsRoutes);
+router.use("/stats", authMiddleware, statsRoutes);
 
 export default router;
