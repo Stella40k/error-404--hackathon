3ac9b8a76ebf8f19b79eeae6dd6d9d5c644ad353
Viviana Gonzalez <vivigz06@gmail.com>
2025-10-17 06:26:28 -0300

    se modificó el role del user

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["User", "Pro"],
+      enum: ["User", "Autoridades"],
       default: "User",
     },
     avatarUrl: {
